7f2723e06f65f549a092c787dd9eb5ed3afc7749
Tryxis <47526388+Tryxis@users.noreply.github.com>
2024-02-13 22:33:49 +0000

    Updated controls and added quit function

--- Downhill Davey/Assets/Scripts/PlayerController.cs
+++ Downhill Davey/Assets/Scripts/PlayerController.cs
@@ -16,17 +16,27 @@ public class PlayerController : MonoBehaviour
         rb2d = GetComponent<Rigidbody2D>();
         surfaceEffector2D = FindObjectOfType<SurfaceEffector2D>();
         Cursor.visible = false;
+        
     }
 
     
     void Update()
     {
-        if(canMove)
+        if (canMove)
         {
             RotatePlayer();
             BoostPlayer();
+
         }
-        
+        QuitGame();
+    }
+
+    void QuitGame()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
     }
 
     public void DisableControls()
@@ -36,7 +46,7 @@ public class PlayerController : MonoBehaviour
 
     void BoostPlayer()
     {
-        if(Input.GetKey(KeyCode.UpArrow))
+        if(Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
         {
             surfaceEffector2D.speed = boostSpeed;
         }
@@ -48,11 +58,11 @@ public class PlayerController : MonoBehaviour
 
     void RotatePlayer()
     {
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
         {
             rb2d.AddTorque(torqueAmount * Time.deltaTime);
         }
-        else if (Input.GetKey(KeyCode.RightArrow))
+        else if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
         {
             rb2d.AddTorque(-torqueAmount * Time.deltaTime);
         }
